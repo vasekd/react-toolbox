51c128ff56b817becb985f92068e0a61395f9980
Vaclav Dovrtel <vaclav.dovrtel@inzata.com>
2019-04-12 13:38:32 +0200

    CODE: Remove proptypes

--- components/date_picker/CalendarMonth.js
+++ components/date_picker/CalendarMonth.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import time from '../utils/time.js';
 import utils from '../utils/utils.js';
 import CalendarDay from './CalendarDay.js';
